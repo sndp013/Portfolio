36b53c165bb2928e2847767a4993f957a6a7710a
Sandeep Jadhav <sndp013@gmail.com>
2026-04-20 22:30:02 +0530

    Fix base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sandeep-jadhav-portfolio/",
+  base: "/",
   plugins: [react()],
 });
